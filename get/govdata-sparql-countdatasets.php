6af9174696daf19538bc637aa3681773a024681b
Thomas Tursics <thomas@tursics.de>
2022-09-02 22:52:54 +0200

    use JSON output (not XML)

--- get/govdata-sparql-countdatasets.php
+++ get/govdata-sparql-countdatasets.php
@@ -5,26 +5,20 @@
 
 	$endpoint = 'https://www.govdata.de/sparql?query=';
 	$sparql = '
-PREFIX dcat: <http://www.w3.org/ns/dcat#>
-SELECT (COUNT(?dataset) AS ?datasets) WHERE {
-  ?dataset a dcat:Dataset .
-}
+		PREFIX dcat: <http://www.w3.org/ns/dcat#>
+		SELECT (COUNT(?dataset) AS ?datasets) WHERE {
+		  ?dataset a dcat:Dataset .
+		}
 	';
 
-    $xml = file_get_contents($endpoint . urlencode($sparql));
+	$opts = [
+		"http" => [
+			"method" => "GET",
+			"header" => "Accept: application/sparql-results+json,*/*;q=0.9\r\n"
+		]
+	];
+	$context = stream_context_create($opts);
 
-	$start = stripos($xml, '<binding name="datasets">');
-	$end = stripos($xml, '</binding>', $start);
-	$length = $end - $start;
-
-	$xml = substr($xml, $start, $length);
-
-	$start = stripos($xml, '<literal', 1);
-	$start = stripos($xml, '>', $start) + 1;
-	$end = stripos($xml, '</literal>', $start);
-	$length = $end - $start;
-
-	$xmlValue = substr($xml, $start, $length);
-
-	echo $xmlValue;
+	$json = json_decode(file_get_contents($endpoint . urlencode($sparql), false, $context));
+	echo $json->results->bindings[0]->datasets->value;
 ?>

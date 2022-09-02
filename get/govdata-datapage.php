28aee511259fbf8c1dcd292796b99ab3500ccbf1
Thomas Tursics <thomas@tursics.de>
2022-09-02 22:20:23 +0200

    fix quotes

--- get/govdata-datapage.php
+++ get/govdata-datapage.php
@@ -1,7 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET');
-    header("Access-Control-Allow-Headers: X-Requested-With");
+    header('Access-Control-Allow-Headers: X-Requested-With');
 
     $html = file_get_contents('https://www.govdata.de/web/guest/daten');
 

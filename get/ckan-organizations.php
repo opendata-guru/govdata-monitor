4a72761ea37cca92f611904875e95d661d3b8142
Thomas Tursics <thomas@tursics.de>
2022-11-14 23:25:40 +0100

    add a mapping to gdi-de.org URIs

--- get/ckan-organizations.php
+++ get/ckan-organizations.php
@@ -5,24 +5,56 @@
 	header('Content-Type: application/json; charset=utf-8');
 
 	$uriCKAN = 'https://ckan.govdata.de';
+	$uriCKAN = 'https://offenedaten.aachen.de';
+	$uriCKAN = 'https://opendata.schleswig-holstein.de';
 	$uri = $uriCKAN . '/api/3/action/organization_list';
+	$uriDomain = end(explode('/',$uriCKAN));
 	$json = json_decode(file_get_contents($uri));
 
+	$mappingFile = 'contributor-uri-map.csv';
+	$mappingList = explode("\n", file_get_contents($mappingFile));
+	$mapping = [];
+
+	array_shift($mappingList);
+	foreach($mappingList as $line) {
+		if ($line != '') {
+			$mapping[] = explode(',', $line);
+		}
+	}
+
 	$data = [];
+
+	function semanticContributor($obj) {
+		global $mapping, $uriDomain;
+
+		$obj['contributor'] = '';
+
+		foreach($mapping as $line) {
+			if ($line[0] == $obj['uri']) {
+				$obj['contributor'] = $line[2];
+			} else if ($line[0] == ($uriDomain . '|' . $obj['name'])) {
+				$obj['contributor'] = $line[2];
+			}
+		}
+
+		unset($obj['uri']);
+
+		return $obj;
+	}
 
 	foreach($json->result as $orgaID) {
 		$uri = $uriCKAN . '/api/3/action/organization_show?id=';
 		$json = json_decode(file_get_contents($uri . $orgaID));
 		$uris = json_decode($json->result->extras[0]->value);
 
-		$data[] = array(
+		$data[] = semanticContributor(array(
 			'id' => $json->result->id,
 			'name' => $json->result->name,
 			'title' => $json->result->title,
 			'created' => $json->result->created,
 			'packages' => $json->result->package_count,
 			'uri' => $uris[0]
-		);
+		));
 	}
 
 	echo json_encode($data);

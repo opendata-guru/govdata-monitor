ece9887e468c800b5e17fc2ff6cf44de0b26009b
Thomas Tursics <thomas@tursics.de>
2023-01-19 07:27:41 +0100

    prepare the catalog table

--- assets/monitor.js
+++ assets/monitor.js
@@ -1,9 +1,49 @@
 var monitor = {
+    data: [],
+    displayCatalogId: '',
+    displayDate: '',
     nextDate: '',
     nextUri: '',
 };
 
+function monitorUpdateCatalogTable() {
+    var data = monitor.data[monitor.displayDate];
+    var table = '';
+
+    if (data) {
+        console.log(data);
+    } else {
+        table += '<tr><td>No data available</td></tr>';
+    }
+
+    document.getElementById('supplier-table').innerHTML = table;
+}
+
 function monitorSetDate(date) {
+    monitor.displayDate = date;
+
+    var text = '';
+    text += '<span class="text-muted">Show date</span>';
+    text += '<span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> ' + monitor.displayDate + ' </span>';
+
+    document.getElementById('display-date').innerHTML = text;
+
+    monitorUpdateCatalogTable();
+}
+
+function monitorSetCatalog(catalogId) {
+    monitor.displayCatalogId = catalogId;
+
+    var text = '';
+    text += '<span class="text-muted">Show catalog</span>';
+    text += '<span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> ' + monitor.displayCatalogId + ' </span>';
+
+    document.getElementById('display-catalog').innerHTML = text;
+
+    monitorUpdateCatalogTable();
+}
+
+function monitorSetNextDate(date) {
     var dateString = date.toISOString().split('T')[0];
     var uri = 'https://opendata.guru/govdata/assets/data-' + dateString.split('-')[0] + '/' + dateString + '-organizations.json';
 
@@ -14,17 +54,31 @@ function monitorSetDate(date) {
 function monitorShowNextDate() {
     var text = '';
     text += '<span class="text-muted">Load data from</span>';
-    text += '<span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> ' + monitor.nextDate + ' </span>';
+    text += '<span class="text-warning"> <i class="mdi mdi-arrow-bottom-right"></i> ' + monitor.nextDate + ' </span>';
 
     document.getElementById('loading-description').innerHTML = text;
+    document.getElementById('loading-icon-progress').style.display = 'block';
+    document.getElementById('loading-icon-done').style.display = 'none';
+}
+
+function monitorShowNextDateDone() {
+    var text = '';
+    text += '<span class="text-muted">Loading data </span>';
+    text += '<span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> done </span>';
+
+    document.getElementById('loading-description').innerHTML = text;
+    document.getElementById('loading-icon-progress').style.display = 'none';
+    document.getElementById('loading-icon-done').style.display = 'block';
 }
 
 function monitorProcessNextData(data) {
     data.forEach(item => {
-        if (item.id === 'govdata.de') {
+        if (item.id === monitor.displayCatalogId) {
             console.log(item);
         }
     });
+    monitorSetDate(monitor.nextDate);
+    monitorShowNextDateDone();
 }
 
 function monitorLoadNextDate() {
@@ -41,9 +95,10 @@ function monitorLoadNextDate() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-//    monitorSetDate(new Date(Date.now() - 5 * 24 * 60 * 60 * 1000));
-    monitorSetDate(new Date(Date.now()));
-    monitorShowNextDate();
+//    monitorSetNextDate(new Date(Date.now() - 5 * 24 * 60 * 60 * 1000));
+    monitorSetNextDate(new Date(Date.now()));
+    monitorSetCatalog('govdata.de');
 
+    monitorShowNextDate();
     monitorLoadNextDate();
 });

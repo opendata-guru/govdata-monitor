f9b5dfa52858a789b48624a69dc8c8b2f25c932b
Thomas Tursics <thomas@tursics.de>
2023-01-29 23:14:01 +0100

    add fallback to scrape website for dataset count

--- get/ckan-count-datasets.php
+++ get/ckan-count-datasets.php
@@ -7,6 +7,7 @@
 	$orgaListSuffix = '/api/3/action/organization_list';
 	$groupListSuffix = '/api/3/action/group_list';
 	$packageShowSuffix = '/api/3/action/package_search?rows=1&start=0';
+	$countWebsiteSuffix = '/search';
 
 	$paramLink = htmlspecialchars($_GET['link']);
 	if ($paramLink == '') {
@@ -19,13 +20,34 @@
 		exit;
 	}
 
-	$uriCKAN = substr($paramLink, 0, -strlen($orgaListSuffix));
+	function scrapeWebsite($uriCKAN) {
+		global $countWebsiteSuffix;
+
+		$uri = $uriCKAN . $countWebsiteSuffix;
+		$source = file_get_contents($uri);
+
+		$html = $source;
+		$start = stripos($html, 'view-header');
+		$end = stripos($html, '</div>', $start);
+		$length = $end - $start;
+		$html = trim(substr($html, $start, $length));
+		$html = explode(' ', $html);
+		$packageCount = $html[count($html) - 2];
+
+		return $packageCount;
+	}
+
+	if ($orgaListSuffix == substr($paramLink, -strlen($orgaListSuffix))) {
+		$uriCKAN = substr($paramLink, 0, -strlen($orgaListSuffix));
+	} else {
+		$uriCKAN = substr($paramLink, 0, -strlen($groupListSuffix));
+	}
 
 	$json = json_decode(file_get_contents($uriCKAN . $packageShowSuffix));
 
 	if ($json) {
 		echo $json->result->count;
 	} else {
-		echo 'null';
+		echo scrapeWebsite($uriCKAN);
 	}
 ?>

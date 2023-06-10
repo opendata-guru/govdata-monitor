a93b73663d7ffc4b781460546a75b636ceef7876
Thomas Tursics <thomas@tursics.de>
2023-06-10 06:08:48 +0200

    quick tries

--- assets/page.datasets.js
+++ assets/page.datasets.js
@@ -1,11 +1,83 @@
 var page = {
+	catalog: '',
+	catalogParent: '',
+    dataURI: 'https://data.europa.eu/api/hub/search/',
+	service: 'piveau',
 };
+
+var params = new URLSearchParams(window.location.search);
+
+if (params.has('cat')) {
+	page.catalog = params.get('cat');
+}
+if (params.has('in')) {
+	page.catalogParent = params.get('in');
+}
+
+// sample:
+// cat=8196e75f-c45e-4281-ac8f-1803c4e1f3be&in=govdata
+// cat=8196e75f-c45e-4281-ac8f-1803c4e1f3be
+
+if (page.catalogParent === 'govdata') {
+	page.service = 'ckan';
+	page.dataURI = 'https://www.govdata.de/ckan/api/action/';
+} else {
+	page.service = 'ckan';
+//	page.dataURI = 'https://opendata.guru/proxy/cors?url=https://mobilithek.info/mobilithek/api/v1.0/export/datasets/dcatapde';
+	page.dataURI = 'https://opendata.guru/proxy/cors?url=https://www.mcloud.de/export/datasets';
+
+//	page.service = MobilithekDistributions;
+//	page.dataURI = 'https://mobilithek.info/mobilithek/api/v1.0/export/datasets/dcatapde?page=0&size=10';
+//	page.dataURI = 'https://mobilithek.info/mobilithek/api/v1.0/export/datasets/dcatapde';
+}
+
+// ----------------------------------------------------------------------------
+/*
+class MobilithekDistributions {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  /**
+   * @description GET a distribution with the given ID.
+   * @returns {Promise}
+   */
+/*  getSingle(id) {
+    return new Promise((resolve, reject) => {
+      const endpoint = 'resource_show';
+      const reqStr = `${this.baseUrl}${endpoint}`;
+      axios.get(reqStr, {
+        params: {
+          id,
+        },
+      })
+        .then((response) => {
+          const dist = response.data.result;
+          const distribution = {};
+          distribution.accessUrl = dist.access_url;
+          distribution.description = dist.description;
+          distribution.downloadUrl = dist.url;
+          distribution.format = dist.format;
+          distribution.id = dist.id;
+          distribution.licence = 'unknown';
+          distribution.modificationDate = dist.last_modified;
+          distribution.releaseDate = dist.created;
+          distribution.title = dist.name;
+          distribution.urlType = dist.url_type;
+          resolve(distribution);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+}*/
 
 // ----------------------------------------------------------------------------
 
 var CONFIG_APP_TITLE = "GovData Monitor Portal",
-    CONFIG_APP_DATA_URL = "https://data.europa.eu/api/hub/search/",
-    CONFIG_APP_DATA_SERVICE = "piveau",
+    CONFIG_APP_DATA_URL = page.dataURI,
+    CONFIG_APP_DATA_SERVICE = page.service,
     CONFIG_APP_DATA_CACHE_BUSTING = !0,
     CONFIG_APP_GAZETTEER_URL = "https://data.europa.eu/api/hub/search/gazetteer/",
     CONFIG_APP_UPLOAD_URL = "https://www.europeandataportal.eu/data/api/",

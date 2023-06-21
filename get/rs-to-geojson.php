2e5b31a458de0964caab76b22018a70d61bbb1f2
Thomas Tursics <thomas@tursics.de>
2023-06-22 00:48:06 +0200

    add a monthly cache for geojson files

--- get/rs-to-geojson.php
+++ get/rs-to-geojson.php
@@ -10,12 +10,45 @@
 		exit;
 	}
 
-	$ars = $paramRS;
-	$output = '&OUTPUTFORMAT=application%2Fjson';
-	$crs = '&srsName=urn:ogc:def:crs:EPSG::4326';
-	$getFeature = '&Service=WFS&Version=2.0.0&Request=GetFeature';
-	$filter = '&FILTER=%3CFilter%3E%3CPropertyIsEqualTo%3E%3CValueReference%3Ears%3C/ValueReference%3E%3CLiteral%3E' . $ars . '%3C/Literal%3E%3C/PropertyIsEqualTo%3E%3C/Filter%3E';
-	$uri = 'https://sgx.geodatenzentrum.de/wfs_vg250?TYPENAMES=vg250:vg250_lan' . $getFeature . $output . $crs . $filter;
+	$paramRS = preg_replace("/[^0-9]/", '', $paramRS);
+	$paramRS = substr($paramRS, 0, 15);
 
-	echo file_get_contents($uri);
+	$filePath = '../assets/geojson-' . date('Y') . '/' . date('Y-m') . '/' . $paramRS . '.geojson';
+
+	function getWorkingData() {
+		global $filePath;
+
+		$dir = dirname($filePath);
+		mkdir($dir, 0777, true);
+
+		$data = file_get_contents($filePath);
+		if (false === $data) {
+			return null;
+		}
+
+		return $data;
+	}
+
+	function setWorkingData($data) {
+		global $filePath;
+
+		file_put_contents($filePath, $data);
+	}
+
+	$data = getWorkingData();
+
+	if (is_null($data)) {
+		$ars = $paramRS;
+		$output = '&OUTPUTFORMAT=application%2Fjson';
+		$crs = '&srsName=urn:ogc:def:crs:EPSG::4326';
+		$getFeature = '&Service=WFS&Version=2.0.0&Request=GetFeature';
+		$filter = '&FILTER=%3CFilter%3E%3CPropertyIsEqualTo%3E%3CValueReference%3Ears%3C/ValueReference%3E%3CLiteral%3E' . $ars . '%3C/Literal%3E%3C/PropertyIsEqualTo%3E%3C/Filter%3E';
+		$uri = 'https://sgx.geodatenzentrum.de/wfs_vg250?TYPENAMES=vg250:vg250_lan' . $getFeature . $output . $crs . $filter;
+
+		$data = file_get_contents($uri);
+
+		setWorkingData($data);
+	}
+
+	echo $data;
 ?>

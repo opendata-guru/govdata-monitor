7cd42b7edca824e02151101f7c5a3d2285a3e584
Thomas Tursics <thomas@tursics.de>
2023-12-14 07:59:46 +0100

    use the entrystore API the correct way

--- get/list-organizations/sachsen-list-organizations.php
+++ get/list-organizations/sachsen-list-organizations.php
@@ -7,7 +7,6 @@
 	include('_semantic.php');
 
 	$sachsenSuffix = 'https://www.opendata.sachsen.de';
-	$max = 1000;
 
 	$paramLink = htmlspecialchars($_GET['link']);
 	if ($paramLink == '') {
@@ -25,19 +24,6 @@
 
 	$query = 'search' .
 		'?type=solr' .
-		'&query=' .
-			'public:true' .
-			'+AND+' .
-			'(rdfType:http%5C%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2FAgent' .
-			'+OR+' .
-			'rdfType:http%5C%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2FOrganization' .
-			'+OR+' .
-			'rdfType:http%5C%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2FPerson)' .
-			'+AND+' .
-			'title:*' .
-		'&limit=' . $max;
-/*	$query = 'search' .
-		'?type=solr' .
 		'&query=(' .
 			'rdfType:http%5C%3A%2F%2Fwww.w3.org%2Fns%2Fdcat%23Dataset' .
 			'+OR+' .
@@ -49,38 +35,53 @@
 			'+OR+' .
 			'title.nolang:*' .
 			')' .
-		'&limit=1' .
-		'&sort=title.de+asc,title.nolang+asc' .
+		'&limit=0' .
 		'&facetFields=' .
-			'metadata.predicate.uri.9259d4c1,' .
-			'metadata.predicate.literal_s.b5d28d0a,' .
-			'metadata.predicate.uri.0ce6b231,' .
-			'metadata.predicate.literal_s.a6424133,' .
-			'metadata.predicate.uri.23fc4665,' .
-			'related.metadata.predicate.uri.3494e2ce,' .
-			'rdfType,' .
-			'metadata.predicate.literal_s.759a4eb7,' .
-			'metadata.predicate.literal_s.759a4eb7,' .
-			'metadata.predicate.uri.2198789d,' .
-			'metadata.predicate.uri.1c06f647';*/
+			'metadata.predicate.uri.9259d4c1';           // organizations
+/*			'metadata.predicate.literal_s.b5d28d0a,' .   // filetype
+			'metadata.predicate.uri.0ce6b231,' .         // groups
+			'metadata.predicate.literal_s.a6424133,' .   // keywords
+			'metadata.predicate.uri.23fc4665,' .         // access rights
+			'related.metadata.predicate.uri.3494e2ce,' . // licenses
+			'rdfType,' .                                 // type
+			'metadata.predicate.literal_s.759a4eb7,' .   // ?
+			'metadata.predicate.uri.2198789d,' .         // ?
+			'metadata.predicate.uri.1c06f647';           // ?
+*/
+
+	function getRessourceQuery($organisation) {
+		$str = str_replace(':', '\:', $organisation);
+		$str = urlencode($str);
+		$query = 'search' .
+			'?type=solr' .
+			'&query=' .
+				'public:true' .
+				'+AND+' .
+				'(resource:' . $str . ')';
+
+		return $query;
+	}
 
 	$json = json_decode(file_get_contents($uri . $query));
-
 	$data = [];
 
-	foreach($json->resource->children as $organisation) {
-		$metadata = (array) reset($organisation->metadata);
-		$title = $metadata['http://xmlns.com/foaf/0.1/name'][0]->value;
-		$info = (array) reset($organisation->info);
-		$created = $info['http://purl.org/dc/terms/created'][0]->value;
+	foreach($json->facetFields[0]->values as $organisation) {
+		$name = $organisation->name;
 
-		$name = preg_replace('#[^a-z0-9]#i', '', $title);
+		$ressourceQuery = getRessourceQuery($name);
+		$ressourceJSON = json_decode(file_get_contents($uri . $ressourceQuery));
+//		$ressourceJSON = file_get_contents($uri . $ressourceQuery);
+
+		$metadata = $ressourceJSON->resource->children[0]->metadata;
+		$nameEntry = get_object_vars(reset($metadata))['http://xmlns.com/foaf/0.1/name'];
+		$title = $nameEntry[0]->value;
+
 		$data[] = semanticContributor($uriDomain, array(
 			'id' => $name,
 			'name' => $name,
 			'title' => $title,
-			'created' => $created,
-			'packages' => '',
+			'created' => '',
+			'packages' => $organisation->count,
 			'uri' => ''
 		));
 	}
